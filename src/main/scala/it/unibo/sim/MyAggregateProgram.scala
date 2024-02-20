147257f247d9b300eaeb2cc8e485245e76a7fcf9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-02-20 12:00:23 +0100

    use BlockC

--- src/main/scala/it/unibo/sim/MyAggregateProgram.scala
+++ src/main/scala/it/unibo/sim/MyAggregateProgram.scala
@@ -4,7 +4,7 @@ import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 
 abstract class MyAggregateProgram
   extends AggregateProgram with 
-  StandardSensors with ScafiAlchemistSupport with BlockG with Gradients with FieldUtils {
+  StandardSensors with ScafiAlchemistSupport with BlockG with BlockC with Gradients with FieldUtils {
 
     def senseOr[T](name: String, otherwise: T): T = if(node.has(name)) sense[T](name) else otherwise
 }
